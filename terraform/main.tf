797b9a532586aa66e7762aa5e9a8aa79ef324cdb
naruse666 <become15@i.softbank.jp>
2024-03-17 22:08:13 +0900

    simple server

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,25 @@ data "sakuracloud_zone" "current" {}
 output "zone" {
   value = data.sakuracloud_zone.current.id
 }
+
+resource "sakuracloud_server" "foobar" {
+  name        = "foobar"
+  disks       = [sakuracloud_disk.foobar.id]
+  description = "description"
+  tags        = ["tag1", "tag2"]
+
+  disk_edit_parameter {
+    hostname        = "hostname"
+    password        = "password"
+    disable_pw_auth = true
+  }
+}
+
+data "sakuracloud_archive" "ubuntu" {
+  os_type = "ubuntu2004"
+}
+
+resource "sakuracloud_disk" "foobar" {
+  name              = "{{ .arg0 }}"
+  source_archive_id = data.sakuracloud_archive.ubuntu.id
+}
